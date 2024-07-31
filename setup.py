0647a87c1c63ba25d06cf5daa729ed36f20d084e
Deadcat <artem.nkfv@gmail.com>
2024-07-31 11:51:18 +0300

    minor changes

--- setup.py
+++ setup.py
@@ -12,7 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py'))
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name), glob('urdf/*.urdf')),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -23,7 +25,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'example_node = duo.example:main'
+            'circle_publisher = duo.circle_publisher:main'
         ],
     },
 )
